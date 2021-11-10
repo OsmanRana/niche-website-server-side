f4780712e977935f1726ab317d22026760747f2c
OsmanRana <osmancmc@yahoo.com>
2021-11-11 01:24:24 +0600

    admin verified

--- index.js
+++ index.js
@@ -19,6 +19,44 @@ async function run() {
         const database = client.db("twelfthAssignmentDb");
         const usersCollection = database.collection("users");
 
+        //find an admin
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin })
+        });
+
+        //send user to the database
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.json(result)
+        });
+
+        //update or insert user
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email }
+            const options = { upsert: true };
+            const updateDoc = { $set: user }
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result)
+        });
+
+        //make an admin 
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result)
+        })
+
     }
     finally {
         // await client.close();
